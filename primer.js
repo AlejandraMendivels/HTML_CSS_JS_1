f611b961430cd235355b3be80523ecd3cfdff84b
AlejandraMendivels <larairan@uniboyaca.edu.co>
2023-04-22 14:42:13 -0500

    Se agrego el segundo ejercicio

--- primer.js
+++ primer.js
@@ -4,3 +4,19 @@ function contarCaracteres() { // Funcion primer ejercicio
     const count = textInput.value.length;// Se utiliza el length para obtener la cantidad de caracteres 
     contar.innerText = `${count} caracteres`;//Aqui muestra los caracteres
   }
+
+
+function mostrarTipoDeDato() {//Funciones segundo ejercicio
+  const textInput = document.getElementById('text-1input').value;
+  const verificar = document.getElementById('verificar');
+  verificar.innerText = tipoDeDato(textInput);
+
+
+  function tipoDeDato(input) {
+    if (isNaN(input)) {
+      return "texto";
+    } else {
+      return "número";
+    }//is Not a Number es la fncion isNaN
+  }
+}
